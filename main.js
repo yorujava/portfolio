e9bea4b7f80c15ee9483e506fe4438a1f32df849
yorujava <gogensanzin@gmail.com>
2024-03-31 20:12:37 +0900

    実験

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-function header(rootDir){
-    $.ajax({
-        url: rootDir + "include/header.html",  // 読み込むHTMLファイル
-        cache: false,
-        async: false,
-        dataType: 'html',
-        success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
-            document.write(html);
-        }
-    });
+function writeHeader(){
+	$.ajax({
+		url:"/include/header.html",
+		cache:false,
+		async:false,
+		success:function(html){
+			document.write(html);
+		}
+	});
 }
 
-function header(rootDir){
-    $.ajax({
-        url: rootDir + "include/header.html",  // 読み込むHTMLファイル
-        cache: false,
-        async: false,
-        dataType: 'html',
-        success: function(html){
-            html = html.replace(/\{\$root\}/g, rootDir); //header.htmlの{$root}を置換
-            document.write(html);
-        }
-    });
+function writeFooter(){
+	$.ajax({
+		url:"/include/footer.html",
+		cache:false,
+		async:false,
+		success:function(html){
+			document.write(html);
+		}
+	});
 }
