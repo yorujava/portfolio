308d1d2c68bce0b2ad91da279a5782180908f328
yorujava <gogensanzin@gmail.com>
2024-04-03 08:49:42 +0900

    ハンバーガーメニュー構築

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ fetch("include/header.html")
 fetch("include/footer.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#footer").innerHTML = data);
+$(function(){
+ 
+    var old_Y = 0;
+    $(window).on('scroll', function (){
+      var new_Y = $(window).scrollTop();
+      var d = old_Y - new_Y;
+      old_Y = new_Y;
+      if ( d > 0 ) {
+        /* to Top */
+        $('.header').slideDown();
+      } else {
+        /* to Bottom */
+        $('.header').slideUp();
+      }
+    });
+   
+  });
