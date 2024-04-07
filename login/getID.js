ddb8feb50aac407454b8c8667b3763af2af10232
yorujava <gogensanzin@gmail.com>
2024-04-07 14:10:09 +0900

    ID作成を仮実装

--- login/getID.js
+++ login/getID.js
@@ -13,7 +13,7 @@ var msg='';
 login.addEventListener("submit",function(){
     const id=idInput.value;
     const pass=passInput.value;
-    if(window.sessionStorage.getItem([id])==pass){
+    if(window.sessionStorage.getItem([id])==null){
     window.sessionStorage.setItem([id],[pass]);
     hid,this.style="display:none";
     msg='ログインしました';
@@ -23,7 +23,7 @@ login.addEventListener("submit",function(){
     loginMsg.appendChild(span);
     window.sessionStorage.setItem(['dspID'],[id]);
     }else{
-        msg='ログインに失敗しました';
+        msg='そのIDは使われています';
         window.sessionStorage.setItem(['loginMessage'],[msg]);
     }
 });
@@ -39,21 +39,26 @@ $(window).on('load', function(){
         loginMsg.appendChild(spanMsg);
     }
     if(window.sessionStorage.getItem(['dspID'])=='ゲスト'){
+        const p0=document.createElement('p');
         const p1=document.createElement('p');
         const p2=document.createElement('p');
         const p3=document.createElement('p');
+        const span0=document.createElement('span');
         const spanId=document.createElement('span');
         const inputId=document.createElement('input');
         const spanPass=document.createElement('span');
         const inputPass=document.createElement('input');
         const button=document.createElement('button');
 
+        span0.textContent='タブを閉じるまで有効なIDを作成します。表示名としてのみ利用されます。日本語も可能です。';
         spanId.textContent='セッションID：';
         inputId.type='text';
         spanPass.textContent='パスワード：';
         inputPass.type='password';
         button.textContent='ログイン';
 
+        p0.appendChild(span0);
+        loginMsg.appendChild(p0);
         p1.appendChild(spanId);
         loginMsg.appendChild(p1);
         loginMsg.appendChild(inputId);
