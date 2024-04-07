7cf5f3550039daecf785108f4e6a73f63c52b623
yorujava <gogensanzin@gmail.com>
2024-04-07 10:28:54 +0900

    ログイン動作更新

--- login/login.js
+++ login/login.js
@@ -6,10 +6,7 @@ const getId=document.getElementById('get-id');
 const logout=document.getElementById('log-out');
 const login=document.getElementById('log-in');
 const loginMsg=document.getElementById('login-msg');
-const guestId='ゲスト';
-const guestPass='1234';
 window.sessionStorage.setItem(['loginMsg'],['ようこそ']);
-window.sessionStorage.setItem([guestId],[guestPass]);
 const hid=document.getElementById('hd');
 var msg='';
 
@@ -23,13 +20,6 @@ login.addEventListener("submit",function(){
     hid,this.style="display:none";
     msg='ログインしました';
     const span=document.createElement('span');
-    const br=document.createElement('br');
-    const a=document.createElement('a');
     span.textContent=msg;
-    a.textContent='トップページ';
-    a.href="https://yorujava.github.io/portfolio/";
     loginMsg.appendChild(span);
-    loginMsg.appendChild(br);
-    loginMsg.appendChild(br);
-    loginMsg.appendChild(a);
 });
